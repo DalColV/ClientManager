9a77443e1a72cb2c9423f25cd6026ad82645980c
Ana Dal Col <carolina.dalcol.documentos@gmail.com>
2025-05-31 22:05:13 -0300

    feat: adiciona domain de cliente

---
+++ backend/src/domain/repositories/Client.repository.ts
@@ -0,0 +1,13 @@
+import { Client } from '../models/Client';
+
+export interface ClientRepository {
+  create(client: Client): Promise<Client>;
+
+  update(client: Client): Promise<Client>;
+
+  delete(id: string): Promise<void>;
+
+  list(): Promise<Client[]>;
+
+  findAll(): Promise<Client[]>;
+}
